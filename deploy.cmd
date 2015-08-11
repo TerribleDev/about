e7556a3474f9ea0f0d63596edc23eadf606238e0
Tommy Parnell <tparnell8@gmail.com>
2015-08-10 22:04:58 -0400

    only install prd mods

--- deploy.cmd
+++ deploy.cmd
@@ -100,7 +100,7 @@ call :SelectNodeVersion
 :: 3. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install --dev
+  call :ExecuteCmd !NPM_CMD! install
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
@@ -108,7 +108,7 @@ IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
 :: 4. Install gulp
 IF EXIST "%DEPLOYMENT_TARGET%\gulpfile.js" (
   pushd "%DEPLOYMENT_TARGET%"
-  call :ExecuteCmd !NPM_CMD! install gulp
+  call :ExecuteCmd !NPM_CMD! install gulp gulp-cache-bust gulp-concat-css gulp-uglify
   call .\node_modules\.bin\gulp publish
   IF !ERRORLEVEL! NEQ 0 goto error
   popd
