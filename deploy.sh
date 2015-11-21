297bbb0090ba02495dad57dd04e32851330dd6fa
Tommy Parnell <tparnell8@gmail.com>
2015-11-21 01:07:58 -0500

    final time

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ git config user.email "tparnell8@gmail.com"
 git config user.name "Tommy Parnell"
 rm -rf components
 rm -rf node_modules
-git add . --force
+git add --all --force
 git commit -m "Deployed to Github Pages"
 git push --force "git@github.com:tparnell8/about.tommyparnell.com.node.git" master:gh-pages
