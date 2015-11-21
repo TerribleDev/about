2b7f851f0969bd8c7310aa0c1e9d639936969acb
Tommy Parnell <tparnell8@gmail.com>
2015-11-21 00:33:23 -0500

    deploy to about

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config user.email "tparnell8@gmail.com"
+git config user.name "Tommy Parnell"
+git add .
+git commit -m "Deployed to Github Pages"
+git push --force "git@github.com:tparnell8/about.tommyparnell.com.node.git" master:gh-pages
