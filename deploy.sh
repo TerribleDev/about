38c5d8129c39911116d13a2f261ec46f23b33b4b
Tommy Parnell <tparnell8@gmail.com>
2015-11-21 00:42:36 -0500

    add all even forced

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config user.email "tparnell8@gmail.com"
 git config user.name "Tommy Parnell"
-git add .
+git add . --force
 git commit -m "Deployed to Github Pages"
 git push --force "git@github.com:tparnell8/about.tommyparnell.com.node.git" master:gh-pages
